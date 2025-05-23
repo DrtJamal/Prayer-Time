c6e745149d2b7fdb8d605f2e13a9626e3b8bba7f
DrtJamal <maj882014u@gmail.com>
2025-05-23 04:21:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ const nextIqamaElement = document.getElementById("next-iqama");
 const nextPrayerElement = document.getElementById("next-prayer");
 
 let prayers = [
-    { name: "Fajr", iqama: "03:50", class: "fajr-row" },
+    { name: "Fajr", iqama: "03:35", class: "fajr-row" },
     { name: "Dhuhr", iqama: "14:00", class: "dhuhr-row" },
     { name: "Asr", iqama: "18:00", class: "asr-row" },
     { name: "Maghrib", iqamaOffset: 6, class: "maghrib-row" },
-    { name: "Isha", iqama: "23:20", class: "isha-row" }
+    { name: "Isha", iqama: "23:40", class: "isha-row" }
 ];
 
 let currentIqamaIndex = null;
