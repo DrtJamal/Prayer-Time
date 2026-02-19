8dac7c02888687615e2a9f1d7425a69f70904bbc
DrtJamal <maj882014u@gmail.com>
2026-02-19 21:41:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let prayers = [
     { name: "Dhuhr", class: "dhuhr-row" },
     { name: "Asr", class: "asr-row" },
     { name: "Maghrib", iqamaOffset: 5, class: "maghrib-row" },
-    { name: "Isha", iqamaOffset: 15, class: "isha-row" }
+    { name: "Isha", class: "isha-row" }
 ];
 
 let weeklyIqama = {}; // Will hold iqama times from JSON
